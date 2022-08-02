dc166eeff2abeeb7337fc2ecc70422cec933fdc2
Jump T. Wong <awong1900@163.com>
2022-08-02 20:39:35 +0800

    feat: init

---
+++ .vuepress/config.js
@@ -0,0 +1,14 @@
+// .vuepress/config.js
+module.exports = {
+  title: "IORI计时器",
+  description: "小孩子都会用的计时器，教练的好帮手",
+  base: "/iori/",
+  themeConfig: {
+    // logo: "/assets/img/logo.png",
+    nav: [
+      { text: "主页", link: "/" },
+      { text: "指南", link: "/guide/" },
+      //   { text: "External", link: "https://google.com" },
+    ],
+  },
+};
